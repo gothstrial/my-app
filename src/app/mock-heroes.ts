72ec7cf8c7494acaee8c7fa86b73ab85b0a8fe97
unknown <gothstrial@yahoo.com>
2018-06-07 12:02:07 -0400

    Angular modifications

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Hero 1' },
+    { id: 2, name: 'Hero 2' },
+    { id: 3, name: 'Hero 13' },
+    { id: 4, name: 'Hero 14' },
+    { id: 5, name: 'Hero 15' },
+    { id: 6, name: 'Hero 16' },    
+    { id: 7, name: 'Hero 17' },
+]
